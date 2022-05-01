3212199d0822f53a2683820b374c84b77648380c
Sofiane Akbly <sofiane.akbly@gmail.com>
2022-05-01 18:56:48 +0200

    Updated: Force tests folder

---
+++ tests/Test.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * laravel-tools
+ * This file contains Test class.
+ *
+ * Created by PhpStorm on 01/05/2022 at 18:56
+ *
+ * @author Sofiakb <contact.sofiakb@gmail.com>
+ */
+class Test
+{
+    
+}
